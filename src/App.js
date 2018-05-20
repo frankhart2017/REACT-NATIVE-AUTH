2a169a0626209e2f68e0d4bc6b79369d7a25ca79
frankhart2017 <sdharchou@gmail.com>
2018-05-20 20:30:38 +0530

    Add loginform and input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyAZsdZGNIM6MrU8dM0VUNdiRC-czGUY5c0',
+        authDomain: 'authentication-ddc5c.firebaseapp.com',
+        databaseURL: 'https://authentication-ddc5c.firebaseio.com',
+        projectId: 'authentication-ddc5',
+        storageBucket: 'authentication-ddc5c.appspot.com',
+        messagingSenderId: '778022821183'
+      });
+    }
+
     render() {
       return (
         <View>
           <Header headerText="Authentication" />
-          <Text>An App!</Text>
+          <LoginForm />
         </View>
-      )
+      );
     }
 }
 
