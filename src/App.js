aafa5478e1adfac62bc778a7168d157fcd472bac
frankhart2017 <sdharchou@gmail.com>
2018-05-20 21:49:19 +0530

    Final Freeze

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Spinner, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
       firebase.initializeApp({
         apiKey: 'AIzaSyAZsdZGNIM6MrU8dM0VUNdiRC-czGUY5c0',
@@ -14,13 +16,36 @@ class App extends Component {
         storageBucket: 'authentication-ddc5c.appspot.com',
         messagingSenderId: '778022821183'
       });
+
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          this.setState({ loggedIn: true });
+        } else {
+          this.setState({ loggedIn: false });
+        }
+      });
+    }
+
+    renderContent() {
+      switch (this.state.loggedIn) {
+        case true:
+          return (
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          );
+        case false:
+          return <LoginForm />;
+        default:
+          return <Spinner size="large" />;
+      }
     }
 
     render() {
       return (
         <View>
           <Header headerText="Authentication" />
-          <LoginForm />
+          {this.renderContent()}
         </View>
       );
     }
